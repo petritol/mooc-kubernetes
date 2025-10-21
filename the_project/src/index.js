04e96d2ce0a04c6da69c0a0f9b1a28c1db5ac800
Petri Tolonen <petri.tolonen@paretosoftware.fi>
2025-10-21 14:12:30 +0300

    Add a simple HTML page serving to express for exercise 1.5

--- the_project/src/index.js
+++ the_project/src/index.js
@@ -1,8 +1,18 @@
 import express from "express";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 
 const PORT = process.env.PORT;
-  
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server started in port ${PORT}`);
 });
